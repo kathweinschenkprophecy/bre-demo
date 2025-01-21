6a1349ef30599baaf141a9631d09fc45bf2921fd
katherine@prophecy.io <katherine@prophecy.io>
2025-01-21 15:55:33 +0000

    Remove PromoCodeRule and update AddRule_PromoOffers to use IdentifyHighValueCustomers in pipelines.

--- bre-orders/pipelines/L0_setup_sample_data/code/l0_setup_sample_data/functions/Rules.py
+++ bre-orders/pipelines/L0_setup_sample_data/code/l0_setup_sample_data/functions/Rules.py
@@ -16,21 +16,3 @@ def IdentifyHighValueCustomers(
         .when((total_order_amount > lit(500)), lit(1))\
         .otherwise(lit(0))\
         .alias("high_value_flag")
-
-@execute_rule
-def PromoCodeRule(
-        spending: Column=lambda: col("spending"), 
-        high_value_flag: Column=lambda: col("high_value_flag"), 
-        is_recent_order: Column=lambda: col("is_recent_order"), 
-        last_purchase_date: Column=lambda: col("last_purchase_date")
-):
-    return when(((spending > lit(700)) & (high_value_flag == lit(1))), lit("25% Discount"))\
-        .when(((spending > lit(500)) & (is_recent_order == lit(1))), lit("Buy one get one free"))\
-        .when((spending > lit(500)), lit("15% discount"))\
-        .when((spending > lit(300)), lit("10% Discount"))\
-        .when(
-          ((last_purchase_date >= lit("2024-12-01")) & (last_purchase_date <= lit("2024-12-31"))),
-          lit("Free two-day shipping")
-        )\
-        .otherwise(lit("5% Discount"))\
-        .alias("promo_offer")
--- bre-orders/pipelines/Sample_BRE_Pipeline/code/sample_bre_pipeline/graph/AddRule_PromoOffers.py
+++ bre-orders/pipelines/Sample_BRE_Pipeline/code/sample_bre_pipeline/graph/AddRule_PromoOffers.py
@@ -7,4 +7,4 @@ from sample_bre_pipeline.config.ConfigStore import *
 from sample_bre_pipeline.functions import *
 
 def AddRule_PromoOffers(spark: SparkSession, SchemaTransform_2: DataFrame) -> DataFrame:
-    return SchemaTransform_2.withColumn(get_alias(PromoCodeRule()), PromoCodeRule())
+    return SchemaTransform_2.withColumn(get_alias(IdentifyHighValueCustomers()), IdentifyHighValueCustomers())
